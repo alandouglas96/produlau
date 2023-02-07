5e13f41d29f7c0b05c3b404ee4125c3f42531115
alandouglas96 <alandouglas96@gmail.com>
2023-02-07 16:39:06 +0100

    First real changess

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
+import { Header } from './components/header'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <h1>PRODU LAU HERE!</h1>
+      <Header />
+      <section>
+        <div>
+          <div>
+            <h3>Project info</h3>
+          </div>
+          <iframe
+            src="https://player.vimeo.com/video/397382446?h=2eab52b806&color=b3b526&byline=0&portrait=0"
+            width="640"
+            height="360"
+            allow="autoplay; fullscreen; picture-in-picture"
+            allowfullscreen
+            frameborder="0"
+          ></iframe>
+          <div>
+            <h3>Iberia, cada día es el primer día.</h3>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div>
+          <div>
+            <h3>Chupa chups, XXL playlist.</h3>
+          </div>
+          <iframe
+            title="TESTINGAL-1"
+            src="https://player.vimeo.com/video/411000287?h=4d6ae4c834"
+            width="640"
+            height="360"
+            allow="autoplay; fullscreen; picture-in-picture"
+            allowfullscreen
+            frameborder="0"
+          ></iframe>
+        </div>
+        <div>
+          <div>
+            <h3>CaixaBank, Insights Center.</h3>
+          </div>
+          <iframe
+            title="TESTINGAL-2"
+            src="https://player.vimeo.com/video/554307374?h=f00a6ce73a"
+            width="640"
+            height="273"
+            allow="autoplay; fullscreen; picture-in-picture"
+            allowfullscreen
+            frameborder="0"
+          ></iframe>
+        </div>
+      </section>
     </div>
   )
 }
---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <header>
+      <div>
+        <div>
+          <h1>Laura Cusí</h1>
+          <h3>Productora</h3>
+        </div>
+        <div>
+          <h2>Work</h2>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export { Header }
